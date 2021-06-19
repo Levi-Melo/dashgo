a1746c15c02af02f3ec9ce049792f77984869902
levi melo <levicontat@outlook.com>
2021-06-19 13:01:16 -0300

    sidebar context

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import { Logo } from "./Logo";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 import { Notification } from "./NotificationsNav";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWide = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWide && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
       {isWide && <SearchBox />}
       <Flex align="center" ml="auto">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import React from "react";
+import { SidebarProvider } from "../contexts/SidebarDrawerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SidebarProvider>
+        <Component {...pageProps} />
+      </SidebarProvider>
     </ChakraProvider>
   );
 }
