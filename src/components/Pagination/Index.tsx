c4b35ff03d3477b53f1bcc2726e9c70306bad0e0
levi melo <levicontat@outlook.com>
2021-06-23 13:16:56 -0300

    static pagination buttons

--- src/components/Pagination/Index.tsx
+++ src/components/Pagination/Index.tsx
@@ -1,7 +1,41 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
+import { Box, Text, Stack } from "@chakra-ui/react";
 import React from "react";
 import { PaginationIten } from "./PaginationIten";
-export function Pagination() {
+
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registerPerPage?: number;
+  currentPage?: number;
+  onPageChange: (Page: number) => void;
+}
+
+function pagesArray(to: number, from: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  registerPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const siblingsCount = 1;
+  const lastPage = Math.floor(totalCountOfRegisters / registerPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? pagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? pagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -14,9 +48,36 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationIten number={1} isCurrent />
-        <PaginationIten number={2} />
-        <PaginationIten number={3} />
+        {currentPage > 1 + siblingsCount && (
+          <>
+            <PaginationIten number={1} />
+            {currentPage > 2 + siblingsCount && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+          </>
+        )}
+        {previousPages.length > 0 &&
+          previousPages.map((p) => {
+            return <PaginationIten key={p} number={p} />;
+          })}
+        <PaginationIten number={currentPage} isCurrent />
+        {nextPages.length > 0 &&
+          nextPages.map((p) => {
+            return <PaginationIten key={p} number={p} />;
+          })}
+
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            <PaginationIten number={lastPage} />
+            {currentPage + 1 + siblingsCount < lastPage && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+          </>
+        )}
       </Stack>
     </Stack>
   );
